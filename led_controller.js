39952cb038bde9db3f735c5be7d707889e369b8c
VIVEK KUMAR <106121146@nitt.edu>
2023-07-17 18:28:51 +0530

    Update led_controller.js

--- led_controller.js
+++ led_controller.js
@@ -5,11 +5,3 @@ singleButton({name:"Led On", action:"digitalWrite",
 singleButton({name:"Led Off", action:"digitalWrite", 
               pin:"0", value:"LOW", bgcolor:"red",
               shape:"rectangle", align:"left", text_color:"black"})
-
-singleButton({name:"Fan On", action:"analogWrite", 
-              pin:"0", value:"230",bgcolor:"#ffa500",
-              shape:"circle", align:"right","text_color":"white" })
-
-singleButton({name:"Fan Off", action:"analogWrite", 
-              pin:"0", value:"0",bgcolor:"#6a5acd",
-              shape:"circle", align:"right", text_color:"white"})
